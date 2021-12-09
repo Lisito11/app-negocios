c6fdb70bb51e0b942ae5125b4aef00212ee85f90
CristianVargas03 <65677401+CristianVargas34@users.noreply.github.com>
2021-12-08 22:05:52 -0400

    Haciendo base para el mapscreen y creando componente map con toda la logica

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import Map from '../components/Map';
 
 export const MapScreen = () => {
     return (
         <View>
             <Text>Map Screen</Text>
+            <Map />
         </View>
     )
 }
