2716a312257d8095ac6e2ae62fc96a6586aeca5b
CristianVargas03 <65677401+CristianVargas34@users.noreply.github.com>
2021-12-09 10:45:24 -0400

    Fixing the marker bugs

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MapView, { Marker } from 'react-native-maps' //TODO Instalar esta dependencia...
 import { StyleSheet, Dimensions } from 'react-native'
+import response from '../utils/data-test'
 
 //? Variable para guardar el size de la pantalla del dispositivo.
 const height = Dimensions.get('window').height
@@ -17,16 +18,18 @@ const Map = () => {
                 longitudeDelta: 70
             }}
         >
-            <MapView.Marker
-                coordinate={{
-                    latitude: 18.5155327,
-                    longitude: -69.8741513,
-                }}
-                title="Marker Official"
-                description="El Marker por default de la aplicacion"
-            />
-            //TODO Aqui ira una iterracion del componente Marker con los datos de los negocios.
+            {response.map(marker => (
+                <MapView.Marker
+                    key={marker.id}
+                    coordinate={{
+                        latitude: marker.latitude,
+                        longitude: marker.longitude,
+                    }}
+                    title={marker.title}
+                    description={marker.description}
 
+                />
+            ))}
 
         </MapView>
     )
---
+++ utils/data-test.js
@@ -0,0 +1,35 @@
+const response = [
+    {
+        id: '1',
+        latitude: 18.4540776,
+        longitude: -69.9505351,
+        title: 'Bella Vista',
+        description: 'Lo popi',
+    },
+    {
+        id: '2',
+        latitude: 40.6976684,
+        longitude: -74.2605632,
+        title: 'New York City',
+        description: 'Lo gringo',
+    },
+    {
+        id: '3',
+        latitude: 18.2078205,
+        longitude: -67.7101477,
+        title: 'Puerto Rico',
+        description: 'Lo Bori',
+    },
+    {
+        id: '4',
+        latitude: 18,
+        longitude: -69,
+        title: 'Lugar Random',
+        description: 'Esta es la descripcion del lugar Random',
+    },
+
+
+
+]
+
+export default response
